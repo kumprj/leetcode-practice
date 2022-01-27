e66bd244c043f08b7a3a92da9b698bedff212c43
Robert Kump <kumprj@miamioh.edu>
2022-01-27 09:59:38 -0500

    Update main.py

--- palindrome/main.py
+++ palindrome/main.py
@@ -1,5 +1,38 @@
 
 def main():
-    pass
+    no_string()
+    as_string()
+
+def as_string():
+    if x < 0:
+        return False
+    word = str(x)
+    word_reverse = word[::-1]
+    if word == word_reverse:
+        return True
+    else:
+        return False
+def no_string():
+    if (x < 0):
+        return False
+    palindromeList = []
+    while x > 0:
+        print(x % 10)
+        palindromeList.append(x % 10)
+        x = x // 10
+
+    i = len(palindromeList)
+
+    while i > 1:
+        ending = palindromeList.pop()
+        beginning = palindromeList.pop(0)
+        i = len(palindromeList)
+
+        if (ending == beginning):
+            continue
+        else:
+            return False
+
+    return True
 if __name__ == "__main__":
     main()
