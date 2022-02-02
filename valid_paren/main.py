d2c5c1ef83050bba2d925ba521cf35ee638a3af4
Robert Kump <kumprj@miamioh.edu>
2022-02-01 20:02:31 -0700

    making some progress here

--- valid_paren/main.py
+++ valid_paren/main.py
@@ -1,5 +1,15 @@
+
 def main():
-    pass
+    s = "()"
+    my_stack = [] # check data type
+    for item in s[::1]:
+        if item == "(" or item == "{" or item == "[":
+            my_stack.push(item) # get proper syntax for pushing to stack
+        else:
+            my_stack.pop() # remove from stack, maybe check if it equals the type that went in?
+    
+    return my_stack # if my_stack is empty, this is a success.
+
 
 if __name__ == "__main__":
     main()
