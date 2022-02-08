2a8eefa922504a7e23861361d5076e4af512c483
Robert Kump <kumprj@miamioh.edu>
2022-02-07 23:44:48 -0500

    push up these tries

--- longestcommonprefix/main.py
+++ longestcommonprefix/main.py
@@ -1,5 +1,21 @@
+
 def main():
-    pass
+    dict = []
+    strs = ["flower","flow","flight"]
+    output = ""
+    count = 0
+    max = 0
+    for item in strs[0[::1]]: # each item in the first element
+        dict[item] = True
+
+    # Loop through rest of the elements. 
+    # # this pseudo code cant be right
+    for item in strs[1[::1]]:
+        if dict[item] == True:
+            count += 1
+        else:
+            if count > max: max = count
+            count = 0
 
 if __name__ == "__main__":
     main()
